1962ee6afccbfb3a507a66062494e184ee5c9926
Ayush Bhagwan Fanse <87874012+AyushFanse@users.noreply.github.com>
2022-06-12 02:31:02 +0530

    WeatherForecasting

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -97,7 +97,7 @@ const ProfileComponent = ({ URL, Key, W_URL }) => {
                         <div className="userShowBottom">
                             <span className="userShowTitle">Account Details</span>
                             {
-                                userData.number
+                                userData.number.length > 0 
                                     ?
                                     (
                                         <div className="userShowInfo">
