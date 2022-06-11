3150a3646ee977774c06ab7ef2b0e5093de6b039
Ayush Bhagwan Fanse <87874012+AyushFanse@users.noreply.github.com>
2022-06-11 13:23:32 +0530

    WeatherForecasting

--- src/Routes.js
+++ src/Routes.js
@@ -1,32 +1,52 @@
-import React from 'react';
+import React from "react";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
-import Home from './Home/Home';
-import Login from './Auth/Login';
-import Signup from './Auth/Signup';
-import Profile from './Profile/Profile';
-import EditUser from './Profile/EditUser';
-import Error from './Error/Error';
+import Home from "./Home/Home";
+import Login from "./Auth/Login";
+import Signup from "./Auth/Signup";
+import Forgot from "./Auth/Forgot";
+import Reset from "./Auth/Reset";
+import Profile from "./Profile/Profile";
+import EditUser from "./Profile/EditUser";
+import Error from "./Error/Error";
 
-const URL = 'https://weather-forecasting-back.herokuapp.com';
-const Key = '08d082b236fd458698d125650210612';
-const W_URL = 'https://api.weatherapi.com/v1/current.json';
+const URL = "https://weather-forecasting-back.herokuapp.com";
+const Key = "08d082b236fd458698d125650210612";
+const W_URL = "https://api.weatherapi.com/v1/current.json";
 let DateNow = new Date(Date.now());
 
 function RouteComponent() {
-    return (
-        <>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/" ><Login URL={URL} /></Route>
-                    <Route exact path="/signup" ><Signup URL={URL} /></Route>
-                    <Route exact path="/home" ><Home URL={URL} W_URL={W_URL} Key={Key} DateNow={DateNow} /></Route>
-                    <Route exact path="/profile" ><Profile URL={URL} W_URL={W_URL} Key={Key} /></Route>
-                    <Route exact path='/profile/:userId' ><EditUser URL={URL} /></Route>
-                    <Route exact path="/*" ><Error /></Route>
-                </Switch>
-            </BrowserRouter>
-        </>
-    )
+  return (
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Login URL={URL} />
+          </Route>
+          <Route exact path='/signup'>
+            <Signup URL={URL} />
+          </Route>
+          <Route exact path='/forgotpassword'>
+            <Forgot URL={URL} />
+          </Route>
+          <Route exact path='/reset/:token'>
+            <Reset URL={URL} />
+          </Route>
+          <Route exact path='/home'>
+            <Home URL={URL} W_URL={W_URL} Key={Key} DateNow={DateNow} />
+          </Route>
+          <Route exact path='/profile'>
+            <Profile URL={URL} W_URL={W_URL} Key={Key} />
+          </Route>
+          <Route exact path='/profile/:userId'>
+            <EditUser URL={URL} />
+          </Route>
+          <Route exact path='/*'>
+            <Error />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default RouteComponent;
